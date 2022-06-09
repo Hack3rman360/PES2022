0afa580929c3fe9952bbd746c0b6c67bc12e80f3
Hack3rman360 <71470165+Hack3rman360@users.noreply.github.com>
2022-06-09 12:20:09 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include "mbed.h"
-#include "iostream"
+#include "hcsr04.h"
+#include "motordriver.h"
+
+
 
 //init
 //IR-Sensorpins:
@@ -14,20 +17,14 @@ DigitalOut MotorlinksAchteruit(D3);
 DigitalOut MotorrechtsVooruit(D4);
 DigitalOut MotorrechtsAchteruit(D7);
 //us-SENSORPINS:
-DigitalOut USsensorlinksTrig(D8);
-DigitalIn USsensorlinksEcho(D9);
-DigitalOut USsensorMiddenBovenTrig(D10);
-DigitalIn USsensorMiddenBovenEcho(D11);
-DigitalOut USsensorMiddenOnderTrig(D12);
-DigitalIn USsensorMiddenOnderEcho(D13);
-DigitalOut USsensorRechtsTrig(D14);
-DigitalIn USsensorRechtsEcho(D15);
+HCSR04 USsensorLinks(D8, D9);
+HCSR04 USsensorMiddenBoven(D10, D11);
+HCSR04 USsensorMiddenOnder(D12, D13);
+HCSR04 USsensorRechts(D14, D15);
 
 //startknop:
 
 AnalogIn startknop(A0);
-
-
 
 bool setup(){
  if (startknop == true) {
@@ -39,12 +36,63 @@ bool setup(){
  }
 }
 
+bool tiencmveraf() {
+    while (true)
+    {
+        float distanceLinks;
+        float distanceMiddenBoven;
+        float distanceMiddenOnder;
+        float distanceRechts;
+
+        distanceLinks = USsensorLinks.distance();
+        distanceMiddenBoven = USsensorMiddenBoven.distance();
+        distanceMiddenOnder = USsensorMiddenOnder.distance();
+        distanceRechts = USsensorRechts.distance();
+        
+        
+        if (distanceLinks < 10 || distanceMiddenBoven < 10 || distanceMiddenOnder < 10 || distanceRechts < 10) {
+            return true;
+        }
+        else{
+            return false;
+        }
+
+        ThisThread::sleep_for(1ms);
+    }
+}
+
+bool vijfcmveraf() {
+    while (true)
+    {
+        float distanceLinks;
+        float distanceMiddenBoven;
+        float distanceMiddenOnder;
+        float distanceRechts;
+
+        distanceLinks = USsensorLinks.distance();
+        distanceMiddenBoven = USsensorMiddenBoven.distance();
+        distanceMiddenOnder = USsensorMiddenOnder.distance();
+        distanceRechts = USsensorRechts.distance();
+        
+        
+        if (distanceLinks < 5 || distanceMiddenBoven < 5 || distanceMiddenOnder < 5 || distanceRechts < 5) {
+            return true;
+        }
+        else{
+            return false;
+        }
+
+        ThisThread::sleep_for(100ms);
+    }
+}
+
+
 int main(){
     
-    if (setup() == true) {
-        while (true) {
+    if (tiencmveraf() == true) {
         
-        }
+        
+        
     }
 }    
 
